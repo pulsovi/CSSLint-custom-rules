2728a06c638c318477fffc4b1af3eb6a0f6b4b47
Puls Sovi <pulsovi@outlook.com>
2019-05-27 09:31:24 +0200

    refactor: using my own coding standards

--- properties-in-lowercase.js
+++ properties-in-lowercase.js
@@ -1,28 +1,27 @@
 /**
-* properties should be lowercase
-*/
-
-
+ * properties should be lowercase
+ */
 CSSLint.addRule({
+  //rule information
+  id: "lowercase-properties",
+  name: "use only lowercase properties",
+  desc: "Properties should be in lowercase.",
+  browsers: "All",
+  //initialization
+  init: function(parser, reporter) {
+    var rule = this;
 
-    //rule information
-    id: "lowercase-properties",
-    name: "use only lowercase properties",
-    desc: "Properties should be in lowercase.",
-    browsers: "All",
-    //initialization
-    init: function(parser, reporter) {
-        var rule = this;
-
-        parser.addListener("property", function(event) {
-            var name = event.property.text;
-
-                                if (/([A-Z])/.test(name)) {
-                                    reporter.report("Properties should be in lowercase", event.line, event.col, rule);// @todo
-                                }
-                            
-
-        });
-    }
-
+    parser.addListener("property", function(event) {
+      event.value.parts.forEach(function(part) {
+        if (part.text != part.text.toLowerCase()) {
+          reporter.report(
+            "Properties should be in lowercase: " + part,
+            event.line,
+            event.col,
+            rule
+          );
+        }
+      });
+    });
+  }
 });
