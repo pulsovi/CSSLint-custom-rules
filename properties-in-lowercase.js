62903705ab55226d0742e7be4dca410bcd8ef79c
Puls Sovi <pulsovi@outlook.com>
2019-07-17 14:07:53 +0200

    feat: no uppercase property name

--- properties-in-lowercase.js
+++ properties-in-lowercase.js
@@ -12,6 +12,13 @@ CSSLint.addRule({
     var rule = this;
 
     parser.addListener("property", function(event) {
+      if (event.property.text !== event.property.text.toLowerCase())
+        reporter.error(
+          "Properties should be in lowercase: " + event.property,
+          event.property.line,
+          event.property.col,
+          rule
+        );
       event.value.parts.forEach(function(part) {
         if (
           part.text != part.text.toLowerCase() &&
